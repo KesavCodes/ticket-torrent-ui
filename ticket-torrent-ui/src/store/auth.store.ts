e6a2b695e18da76f8c74298059c8519cd62b8517
KesavCodes <kesavakumarcode@outlook.com>
2024-10-23 23:29:59 +0530

    [Add Auth store using Zustand

---
+++ ticket-torrent-ui/src/store/auth.store.ts
@@ -0,0 +1,24 @@
+import { create } from "zustand";
+
+interface AuthStore {
+  user: string | null;
+  login: (id: string) => void;
+  logout: () => void;
+}
+
+export const useAuthStore = create<AuthStore>((set) => {
+  let userTt = localStorage.getItem("userTt");
+  if (userTt) userTt = JSON.parse(userTt);
+  return {
+    user: "",
+    login: (id: string) => {
+      if (!id) return;
+      localStorage.setItem("userTt", JSON.stringify(id));
+      set({ user: id });
+    },
+    logout: () => {
+      localStorage.removeItem("userTt");
+      set({ user: null });
+    },
+  };
+});
